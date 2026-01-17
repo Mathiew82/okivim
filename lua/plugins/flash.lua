bdf6bb9fd4cd0bb416e94e19002ea8b649bb7680
Mathiew82 <amateo1982@yahoo.es>
2026-01-17 22:47:18 +0100

    Add flash plugin to improve the search words

---
+++ lua/plugins/flash.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    config = function()
+      local flash = require("flash")
+
+      -- Keymaps
+      vim.keymap.set({ "n", "x", "o" }, "zk", flash.jump, { desc = "Flash jump" })
+      vim.keymap.set({ "n", "x", "o" }, "Zk", flash.treesitter, { desc = "Flash Treesitter" })
+      vim.keymap.set("o", "r", flash.remote, { desc = "Remote flash" })
+      vim.keymap.set({ "o", "x" }, "R", flash.treesitter_search, { desc = "TS search" })
+      vim.keymap.set("c", "<C-s>", flash.toggle, { desc = "Toggle flash search" })
+    end,
+  },
+}
