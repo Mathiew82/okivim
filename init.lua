39628deedbd1204169b36dd0069945ddb2cd70e7
Mathiew82 <amateo1982@yahoo.es>
2026-01-20 11:27:06 +0100

    Bootstrap to install when lazy.nvim does not exist

--- init.lua
+++ init.lua
@@ -2,7 +2,17 @@ local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 require("config")
 
-if vim.fn.isdirectory(lazypath) == 1 then
-  vim.opt.rtp:prepend(lazypath)
-  require("lazy").setup("plugins")
+if vim.fn.isdirectory(lazypath) == 0 then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
 end
+
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup("plugins")
